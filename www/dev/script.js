2749a98fee253cf014e9cd5e8f058567bedf3f66
Noel Perland <noel.perland@gmail.com>
2022-10-18 22:33:40 +0200

    Har gjort dropdown som fungerar i console med en fet array

--- www/dev/script.js
+++ www/dev/script.js
@@ -1,21 +1,29 @@
-function getContentFromApi(filename, selector) {
-    fetch('http://localhost:3000/api/content/' + filename)
-    .then(response => response = response.json())
-    .then((content) => {
-        console.log(content)
-        document.querySelector(selector).innerHTML = marked.parse(content.data) // marked är ett externt bibliotek som hanterar markdown filer (.md)
-        hljs.highlightAll(); // hljs (highlight.js) ger syntax-highlight i <code>-taggar.
-    })
+const allYellowMembers = [
+  { firstname: "Noel", lastname: "Perland", personalityType: "yellow" },
+  { firstname: "Alex", lastname: "Saxena", personalityType: "yellow" },
+  { firstname: "Knatte", lastname: "Larsson", personalityType: "yellow" },
+];
+const allRedMembers = [
+  { firstname: "Vadzim", lastname: "Kulikouski", personalityType: "red" },
+  { firstname: "Sanaz", lastname: "Effati", personalityType: "red" },
+  { firstname: "Fnatte", lastname: "Larsson", personalityType: "red" },
+];
+const allBlueMembers = [
+  { firstname: "Peter", lastname: "Karlsson", personalityType: "blue" },
+  { firstname: "Linus", lastname: "Härén", personalityType: "blue" },
+  { firstname: "Tjatte", lastname: "Larsson", personalityType: "blue" },
+];
+const allGreenMembers = [
+  { firstname: "Tom", lastname: "Johansson", personalityType: "green" },
+  { firstname: "Gustav", lastname: "Franchell", personalityType: "green" },
+  { firstname: "Latte", lastname: "Larsson", personalityType: "green" },
+];
+
+function getMembersByColor(color) {
+  var makeColor = document.getElementById("color").value;
+  console.log(makeColor);
+  if (makeColor === "yellow") console.log(allYellowMembers);
+  else if (makeColor === "red") console.log(allRedMembers);
+  else if (makeColor === "blue") console.log(allBlueMembers);
+  else if (makeColor === "green") console.log(allGreenMembers);
 }
-// getContentFromApi('kursplan.md', '[data-source="content"]')
-
-function getUsers() {
-    fetch('http://localhost:3000/api/users/')
-        .then((response) => response = response.json())
-        .then(users => {
-            console.log(users)
-        })
-}
-getUsers()
-
-
